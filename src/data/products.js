4c2a437fe623104cb9036e0934c6ac032497cc0b
niro <roquenikko@gmail.com>
2021-04-30 13:01:39 -0700

    Initial Commit

---
+++ src/data/products.js
@@ -0,0 +1,58 @@
+export const products = [
+    {
+        id: 0,
+        price: '$5.99',
+        name: 'Shoes',
+        description: 'Running Shoes',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 1,
+        price: '$5.99',
+        name: 'Macbook',
+        description: 'Apple Laptop',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 2,
+        price: '$5.99',
+        name: 'Samsung S9+',
+        description: 'Samsung Phone',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 3,
+        price: '$5.99',
+        name: 'Gibson',
+        description: 'Electric Guitar',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 4,
+        price: '$5.99',
+        name: 'Remo',
+        description: 'Drums',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 5,
+        price: '$5.99',
+        name: 'Pomsky',
+        description: 'Pomeranian Husky Mix',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 6,
+        price: '$5.99',
+        name: 'G613',
+        description: 'Logitech Wireless Keyboard',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+    {
+        id: 7,
+        price: '$5.99',
+        name: 'Aquafina',
+        description: 'Bottled Water',
+        image: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80'
+    },
+]
